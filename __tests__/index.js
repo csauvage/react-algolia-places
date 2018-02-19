18126ec16ec4476cfdf98a48f40878e2b030d4c7
Clément SAUVAGE <clement.sauvage@me.com>
2018-02-19 23:41:54 +0100

    Update config files

---
+++ __tests__/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Enzyme, { shallow, mount, render } from 'enzyme';
+
+import Place from '../src/components/Place'
+
+
+
+
---
+++ src/Place.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import place from 'places.js';
+
+class Places extends Component {
+    
+  componentDidMount() {
+        
+    let options = {
+      container: this.autoCompletePlace,
+      language: this.props.language,
+      useDeviceLocation: this.props.useDeviceLocation,
+            
+    };
+        
+    const optionnalPropsKeys = ['type', 'countries', 'aroundLatLng', 'aroundRadius', 'templates', 'appId', 'apiKey', 'apiKey'];
+        
+    for (let optionnalPropKey of optionnalPropsKeys) {
+      if (this.props[optionnalPropKey]) { options[optionnalPropKey] = this.props[optionnalPropKey];}
+    }
+        
+    const autocomplete = place(options);
+    autocomplete.on('change', this.props.onChange);
+  }
+    
+  render() {
+    return (
+      <input
+        disabled={this.props.disabled}
+        ref={(input) => {this.autoCompletePlace = input;}}
+      />
+    );
+  }
+}
+
+Places.propTypes = {
+  apiKey: PropTypes.string,
+  appId: PropTypes.string,
+  aroundLatLng: PropTypes.string,
+  aroundRadius: PropTypes.number,
+  countries: PropTypes.arrayOf(PropTypes.string),
+  disabled: PropTypes.bool,
+  language: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  templates: PropTypes.object,
+  type: PropTypes.oneOf(['city', 'country', 'address', 'busStop', 'trainStation', 'townhall', 'airport']),
+  useDeviceLocation: PropTypes.bool,
+};
+
+Places.defaultProps = {
+  disabled: false,
+  language: navigator.language,
+  useDeviceLocation: false,
+  onChange: (query, rawAnswer, suggestion, suggestionIndex) => console.log(query, rawAnswer, suggestion, suggestionIndex),
+    
+};
+
+export default Places;
